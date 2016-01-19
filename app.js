e10173d2e43edccc3d2a62ab146cf0a20f5569b9
danieladler <weston.adler@gmail.com>
2016-01-19 15:16:47 -0800

    add view and stylesheet

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
 
+// view engine
+app.set('view engine', 'jade');
+
+// access to public directory
+app.use(express.static('public'));
+
+// routes
 app.get("/", function (req, res) {
-  res.send("hello!");
+  res.render('index', {title: "dadler codes", message: "content here"})
 });
 
 app.listen(3000, function() {
-  console.log("incoming request");
+
 });
