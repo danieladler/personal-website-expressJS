f86cca14ef9f85154cfeb589101e0ab58463a758
danieladler <weston.adler@gmail.com>
2016-01-22 10:47:14 -0800

    update app.js to run on Heroku port (production) || localhost (development)

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.get("/", function (req, res) {
   res.render('index', {title: "dadler codes", message: "dadler codes"})
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
 
 });
